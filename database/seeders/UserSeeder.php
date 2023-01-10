21fc35fe582e7abdb136c1c1264cec51497dcb70
itsvannnss <ivansetiawan7102@gmail.com>
2023-01-11 01:15:35 +0700

    seeders tambahan

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,7 +20,7 @@ class UserSeeder extends Seeder
         DB::table('users')->insert([
             'nama' => 'Muhammad Ayyub Ramli',
             'email' => 'ayyub@student.telkomuniversity.ac.id',
-            'password' => bcrypt('1'),
+            'password' => bcrypt('ayyub'),
             'role' => 'mahasiswa',
             'remember_token' =>  Str::random(60)
         ]);
@@ -40,11 +40,51 @@ class UserSeeder extends Seeder
             'role' => 'mahasiswa',
             'remember_token' =>  Str::random(60)
         ]);
+
+        DB::table('users')->insert([
+            'nama' => 'Anugrah Bagas',
+            'email' => 'bagas@student.telkomuniversity.ac.id',
+            'password' => bcrypt('bagas'),
+            'role' => 'mahasiswa',
+            'remember_token' =>  Str::random(60)
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => 'Nur Afni',
+            'email' => 'afni@student.telkomuniversity.ac.id',
+            'password' => bcrypt('afni'),
+            'role' => 'mahasiswa',
+            'remember_token' =>  Str::random(60)
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => 'Amanda Aurelie',
+            'email' => 'manda@student.telkomuniversity.ac.id',
+            'password' => bcrypt('manda'),
+            'role' => 'mahasiswa',
+            'remember_token' =>  Str::random(60)
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => 'Ni putu Diah',
+            'email' => 'diah@student.telkomuniversity.ac.id',
+            'password' => bcrypt('diah'),
+            'role' => 'mahasiswa',
+            'remember_token' =>  Str::random(60)
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => 'Andi Aswan',
+            'email' => 'aswan@student.telkomuniversity.ac.id',
+            'password' => bcrypt('aswan'),
+            'role' => 'mahasiswa',
+            'remember_token' =>  Str::random(60)
+        ]);
         
         DB::table('users')->insert([
             'nama' => 'ASTACALA',
             'email' => 'astacala@ukm.telkomuniversity.ac.id',
-            'password' => bcrypt('1'),
+            'password' => bcrypt('astacala'),
             'role' => 'ukm',
             'remember_token' =>  Str::random(60)
         ]);
@@ -61,6 +101,46 @@ class UserSeeder extends Seeder
             'nama' => 'IMABEST',
             'email' => 'imabest@ukm.telkomuniversity.ac.id',
             'password' => bcrypt('imabest'),
+            'role' => 'ukm',
+            'remember_token' =>  Str::random(60)
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => 'KBSU',
+            'email' => 'kbsu@ukm.telkomuniversity.ac.id',
+            'password' => bcrypt('kbsu'),
+            'role' => 'ukm',
+            'remember_token' =>  Str::random(60)
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => 'pmi',
+            'email' => 'pmi@ukm.telkomuniversity.ac.id',
+            'password' => bcrypt('pmi'),
+            'role' => 'ukm',
+            'remember_token' =>  Str::random(60)
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => 'kmk',
+            'email' => 'kmk@ukm.telkomuniversity.ac.id',
+            'password' => bcrypt('kmk'),
+            'role' => 'ukm',
+            'remember_token' =>  Str::random(60)
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => 'Nippon',
+            'email' => 'nippon@ukm.telkomuniversity.ac.id',
+            'password' => bcrypt('nippon'),
+            'role' => 'ukm',
+            'remember_token' =>  Str::random(60)
+        ]);
+
+        DB::table('users')->insert([
+            'nama' => 'BTS',
+            'email' => 'bts@ukm.telkomuniversity.ac.id',
+            'password' => bcrypt('bts'),
             'role' => 'ukm',
             'remember_token' =>  Str::random(60)
         ]);
